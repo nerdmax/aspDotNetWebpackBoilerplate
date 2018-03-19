a1ed3ceef2e826671fd9516c33b3daf78dd826b7
mrliucan <mrliucan@foxmail.com>
2018-03-19 17:01:47 +1100

    Seprate js, scss, font files

---
+++ src/font/main.js
@@ -0,0 +1,6 @@
+// Inject fontawesome
+import fontawesome from "@fortawesome/fontawesome";
+// import solid from '@fortawesome/fontawesome-free-solid';
+import regular from "@fortawesome/fontawesome-free-regular";
+// Add the icon to the library so you can use it in your page
+fontawesome.library.add(regular);
---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+// Inject fundamental libs
+import _ from "lodash";
+import numeral from "numeral";
+import moment from "moment";
+
+// Inject jQuery related libs
+import $ from "jquery";
+import "jquery-match-height";
+import "parsleyjs";
+
+import "bootstrap";
+import "bootstrap-datepicker";
+
+// Below are test code, should delete them later
+
+console.log("DSLKJLJF");
+console.log(numeral);
+console.log(moment);
+
+function component() {
+  var element = document.createElement("div");
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  return element;
+}
+
+window.onload = function() {
+  document.body.appendChild(component());
+};
+
+$(function() {
+  console.log("SDFJdK");
+  $(".match-height").matchHeight();
+});
+
+$(function() {
+  $(".date-picker-test").datepicker({
+    minViewMode: 1,
+    daysOfWeekDisabled: "0",
+  });
+});
